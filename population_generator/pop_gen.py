6b6ca074db2532c7b5599f1f19f7ed7ba24df231
Gunhee Lee <teuksu00@gmail.com>
2022-09-08 20:01:22 +0900

    Added pop_gen.py

---
+++ population_generator/pop_gen.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+
+def main():
+    print("====== Population Generator v0.1 ======")
+    print("numpy version: " + np.__version__)
+    print("pandas version: " + pd.__version__)
+
+if __name__ == "__main__":
+    main()
